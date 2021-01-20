c56e613741a211d9fe19382df6d1bd9beecf0c8e
juniperhaven <alexianfireflies@gmail.com>
2021-01-20 14:52:31 -0800

    added switch statement for days

--- scripts/daily-grind.js
+++ scripts/daily-grind.js
@@ -9,4 +9,38 @@
 let myDate = new Date();
 let myDay = myDate.getDay();
 
+switch(myDay) {
+    case 0:
+        alert("It's Sunday!");
+        break;
+
+    case 1:
+        alert("It's Monday!");
+        break;
+
+    case 2:
+        alert("It's Tuesday!");
+        break;
+
+    case 3:
+        alert("It's Wednesday!");
+        break;
+
+    case 4:
+        alert("It's Thursday!");
+        break;
+    
+    case 5:
+        alert("It's Friday!");
+        break;
+
+    case 6:
+        alert("It's Saturday!");
+        break;
+    
+    default:
+        alert("Something has gone terribly wrong. Please contact my maker.");
+        break;
+}
+
 alert("I'm in a different folder and file now!");
