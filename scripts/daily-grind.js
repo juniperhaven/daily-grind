aafd0b5b0a53f83d35cb6aa8a8102985d20dcce5
juniperhaven <alexianfireflies@gmail.com>
2021-01-20 14:50:33 -0800

    added styling to test.html

--- scripts/daily-grind.js
+++ scripts/daily-grind.js
@@ -1,1 +1,12 @@
+/* things we need to change for each coffee:
+- image source
+- alt tag
+- day of the week
+- name of coffee
+- color
+- description */
+
+let myDate = new Date();
+let myDay = myDate.getDay();
+
 alert("I'm in a different folder and file now!");
