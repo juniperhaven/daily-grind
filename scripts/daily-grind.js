bb6a239885820fabc4851019057b0d29125b9e81
juniperhaven <alexianfireflies@gmail.com>
2021-01-20 15:13:39 -0800

    added coffee object

--- scripts/daily-grind.js
+++ scripts/daily-grind.js
@@ -1,55 +1,115 @@
 /* things we need to change for each coffee:
-- image source
-- alt tag
-- day of the week
-- name of coffee
-- color
-- description */
+name - name of coffee
+pic - image source
+alt - alt tag
+color - color
+desc - description
+day - day of the week */
 
 let myDate = new Date();
 let myDay = myDate.getDay();
 let today = "";
+let coffee = "";
 
 switch(myDay) {
     case 0:
-        alert("It's Sunday!");
+        // alert("It's Sunday!");
         today = "Sunday";
+        coffee = {
+            name: "who knows yet",
+            pic: "pic-src",
+            alt: "alt",
+            color: "color",
+            desc: `desc`,
+            day: "Sunday"
+        };
         break;
 
     case 1:
-        alert("It's Monday!");
+        // alert("It's Monday!");
         today = "Monday";
+        coffee = {
+            name: "who knows yet",
+            pic: "pic-src",
+            alt: "alt",
+            color: "color",
+            desc: `desc`,
+            day: "Monday"
+        };
         break;
 
     case 2:
-        alert("It's Tuesday!");
+        // alert("It's Tuesday!");
         today = "Tuesday";
+        coffee = {
+            name: "who knows yet",
+            pic: "pic-src",
+            alt: "alt",
+            color: "color",
+            desc: `desc`,
+            day: "Tuesday"
+        };
         break;
 
     case 3:
-        alert("It's Wednesday!");
+        // alert("It's Wednesday!");
         today = "Wednesday";
+        coffee = {
+            name: "Drip Coffee",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            color: "burlywood",
+            desc: `desc`,
+            day: "Wednesday"
+        };
         break;
 
     case 4:
-        alert("It's Thursday!");
+        // alert("It's Thursday!");
         today = "Thursday";
+        coffee = {
+            name: "who knows yet",
+            pic: "pic-src",
+            alt: "alt",
+            color: "color",
+            desc: `desc`,
+            day: "Thursday"
+        };
         break;
     
     case 5:
-        alert("It's Friday!");
+        // alert("It's Friday!");
         today = "Friday";
+        coffee = {
+            name: "who knows yet",
+            pic: "pic-src",
+            alt: "alt",
+            color: "color",
+            desc: `desc`,
+            day: "Friday"
+        };
         break;
 
     case 6:
-        alert("It's Saturday!");
+        // alert("It's Saturday!");
         today = "Saturday";
+        coffee = {
+            name: "who knows yet",
+            pic: "pic-src",
+            alt: "alt",
+            color: "color",
+            desc: `desc`,
+            day: "Saturday"
+        };
         break;
     
     default:
         alert("Something has gone terribly wrong. Please contact my maker.");
         today = "404 error today not found";
+        coffee = {};
         break;
 }
+
+console.log(coffee);
 
 // alert("I'm in a different folder and file now!");
